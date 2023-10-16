bd0dd216b9a44ec73dc5d61138141872ad2afb58
tqt-builder <105571224+tqt-builder@users.noreply.github.com>
2023-10-17 00:51:15 +0800

    Update to toan truong (#4)

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,17 +5,12 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'}
+    description={"Toan Truong's personal website."}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
-          </p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
@@ -25,7 +20,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source available <a href="https://github.com/tqt-builder/personal-site">here</a>.</p>
     </article>
   </Main>
 );
