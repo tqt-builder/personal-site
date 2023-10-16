c693fbe8d1f3e7fb051235fbf192f58e74d6161c
tqt-builder <toantq995@gmail.com>
2023-10-17 01:02:09 +0800

    Update mail

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -13,17 +13,13 @@ const SideBar = () => (
       </Link>
       <header>
         <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <p><a href="mailto:toanbkhn95@gmail.com">toanbkhn95@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Toan. I am a software engineer.
       </p>
       <ul className="actions">
         <li>
@@ -34,7 +30,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">Toan Truong<Link to="/">toantruong.me</Link>.</p>
     </section>
   </section>
 );
